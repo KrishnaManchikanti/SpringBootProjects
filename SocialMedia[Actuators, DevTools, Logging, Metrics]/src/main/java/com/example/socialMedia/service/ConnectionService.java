ac4d57307a699cfa91629c00575b48f7ecb8593d
krishna <mkrishna_1998@yahoo.com>
2024-08-08 22:43:50 +0530

    Logging and Metrics(Docker) [levels, hierarchy, saving logs],[Metrics->Micrometer->Prometheus->Grafana]

--- SocialMedia[Actuators, DevTools, Logging, Metrics]/src/main/java/com/example/socialMedia/service/ConnectionService.java
+++ SocialMedia[Actuators, DevTools, Logging, Metrics]/src/main/java/com/example/socialMedia/service/ConnectionService.java
@@ -3,6 +3,10 @@ package com.example.socialMedia.service;
 import java.util.List;
 import java.util.Optional;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +15,21 @@ import com.example.socialMedia.repository.ConnectionDal;
 
 @Service
 public class ConnectionService {
-	
+
+	private Counter connectionCallCounter = null;
+
+	public ConnectionService(CompositeMeterRegistry compositeMeterRegistry) {
+		this.connectionCallCounter = compositeMeterRegistry.counter("connection.call.counter");
+	}
+
 	@Autowired
 	ConnectionDal connectionDao;
 
-	public List<Connection> getConnections() {
+	Logger logger = LoggerFactory.getLogger(ConnectionService.class);
 
+	public List<Connection> getConnections() {
+		logger.trace("TRACING: Connections are on the way....");
+		connectionCallCounter.increment();
 		return connectionDao.findAll();
 	}
 
